19d0fed0489b221e17a4ea6db1e5c84f3c1c5ba8
Richie Hughes <me@richiehughes.co.uk>
2016-03-21 20:10:39 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var notifier = require('node-notifier');
+var path = require('path');
+
+notifier.notify({
+  title: 'My awesome title',
+  message: 'Hello from node, Mr. User!',
+  sound: true, // Only Notification Center or Windows Toasters
+  wait: true // Wait with callback, until user action is taken against notification
+}, function (err, response) {
+  // Response is response from notification
+});
+
+notifier.on('click', function (notifierObject, options) {
+  // Triggers if `wait: true` and user clicks notification
+  console.log(notifierObject);
+});
+
+notifier.on('timeout', function (notifierObject, options) {
+  // Triggers if `wait: true` and notification closes
+});
